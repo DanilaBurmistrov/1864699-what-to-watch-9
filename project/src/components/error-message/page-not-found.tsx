891d70c53e2bf61d1e6d99a87b1a9329e7618a67
Danila Burmistrov <burmistrov.danila2016@yandex.ru>
2022-04-03 22:03:50 +0300

    Добро пожаловать, или посторонним вход воспрещён часть 2, оптимизируй это часть 2

---
+++ project/src/components/error-message/page-not-found.tsx
@@ -0,0 +1,18 @@
+import Logo from '../pages/logo/logo';
+import LogoFooter from '../pages/logo/logo-footer';
+
+export default function PageNotFound(): JSX.Element {
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <Logo />
+      </header>
+      <div>
+        <div style={{marginBottom: 40}}>
+          <h1 className="page-title" style={{textAlign: 'center'}}>404 Page Not Found</h1>
+        </div>
+      </div>
+      <LogoFooter />
+    </div>
+  );
+}
